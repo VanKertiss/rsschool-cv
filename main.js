f616f2a1ba93143397c10cd5794dd89f3b17a7db
VanKertiss <56919897+VanKertiss@users.noreply.github.com>
2021-09-20 19:43:35 +0300

    feat: add adaptive and repair css

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const navLink = document.getElementById('links');
 const linkContent = document.querySelectorAll('.link');
 
-
-
 navLink.addEventListener('click', e => {
     console.log(navLink)
     const currLink = e.target.dataset.btn;
-    for(let i = 0; i< linkContent.length; i++){
+    for (let i = 0; i < linkContent.length; i++) {
         linkContent[i].classList.remove('link-active')
-        if(linkContent[i].dataset.btn === currLink){
+        if (linkContent[i].dataset.btn === currLink) {
             linkContent[i].classList.add('link-active');
         }
     }
 })
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.nav');
+
+menuButton.addEventListener('click', click)
+
+function click() {
+    menuButton.classList.toggle('menu-button-active')
+    menu.classList.toggle('nav-active');
+    console.log('клик')
+}
